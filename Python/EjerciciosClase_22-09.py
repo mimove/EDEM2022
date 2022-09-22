444abc02300bb4f7fd0ba2ad68d9259368c35a8b
mimove <mimove14@gmail.com>
2022-09-22 21:00:44 +0200

    update clase 22 sep 2022

--- Python/EjerciciosClase_22-09.py
+++ Python/EjerciciosClase_22-09.py
@@ -104,4 +104,172 @@ print(f'''Los datos introducidos son:
 
 
 
+
+
+
+#  Operadores, condiciones y bucles
+
+
+
+'''
+PALABRAS RESERVADAS DE PYTHON
+
+- and (operador logico para concatenar condiciones)
+- as ()
+- assert
+- break (para salir de un loop)
+- class (para definir un tipo propio. Por ejemplo tipo persona con atributos nombre, apellidos, email, etc. Además tiene unas acciones (o métodos) como comer, caminar, etc.)
+- continue (para salir de un bucle)
+- def (para definir funciones)
+- del
+- elif (comprobar otra condición despues de un if)
+- else (si ninguna condición se cumple realizar lo siguiente a else)
+- except (si falla el codigo de try hacer lo de except)
+- exec
+- finally (si queremos que si o si se ejecute algo cuando acaba el codigo)
+- for (para empezar un bucle)
+- global (para definir una variable global para todos los ámbitos)
+- if (para definir una condicion)
+- import (para importar modulos que se exportan de una libreria)
+- in (quiere decir dentro de algo que es iterable)
+- is (sirve para saber si un valor es de un tipo u otro)
+- lambda 
+- nonlocal
+- not (para negar una condición)
+- or (operador logico para definir que se cumplan una condición u otra)
+- pass (para que se salte una iteración)
+- raise
+- return (para definir lo que queremos que sea el output de una función)
+- try (prueba a ejecutar lineas de codigo y ver si da error)
+- while (ejecuta el bucle mientras se cumpla cierta condición)
+- with 
+- yield
+- True
+- False
+- None
+
+'''
+
+
+
+a = 0 
+b = 1
+c = 3
+
+
+if (a == 0 or b == 1) and c == 3:
+  nombre = "martin"
+
+print(nombre)
+
+
+
+def suma(a,b):
+  return a + b
+
+valor1 = suma(5,2)
+valor2 = suma(3,2)
+
+print(f'La suma es {valor1} {valor2}')
+
+
+a = 1
+while a == 1:
+  print('yeah')
+  a = 2
+
+
+numeroElegido = int(input('Elige un numero: '))
+numeroBuscado = 300
+intento = 3
+
+while (numeroElegido != numeroBuscado) and intento > 1:
+  if numeroElegido > numeroBuscado :
+    intento -= 1
+    if intento == 1:
+       numeroElegido = int(input(f'Elige otro numero más pequeño, te queda {intento} intento : '))
+    else:
+      numeroElegido = int(input(f'Elige otro numero más pequeño, te quedan {intento} intentos : '))
     
+  elif numeroElegido < numeroBuscado :
+    intento -= 1
+    if intento == 1:
+       numeroElegido = int(input(f'Elige otro numero más grande, te queda {intento} intento : '))
+    else:
+      numeroElegido = int(input(f'Elige otro numero más grande, te quedan {intento} intentos : '))
+    
+if intento < 0:
+  print('Te has quedado sin intentos')
+else:    
+  print(f'Has ganado, el numero era {numeroBuscado}')
+  
+  
+operacion_compleja = 33 * 10 + 2 / 5 ** 2
+print(f"> Operación 33*10+2/5**2 = {operacion_compleja}")
+
+
+nombre: str = "Martín"
+apellidos: str = "San José de Vicente"
+
+# Concatenación
+nombre_completo: str = nombre + " " + apellidos
+print(f"> Nombre completo: {nombre_completo}")  
+
+# Repetición
+nombre_x5: str = nombre*5
+print(f"> Nombre 5 veces: {nombre_x5}")
+
+c: int = 3
+d: int = 3
+e: int = 4
+
+# Igualdad - Nos devolverá True o False
+print(f"> ¿3 y 3 son iguales? {c is d}")
+# Operador Identidad
+print(f"> ¿3 y 3 son iguales? {c == d}")
+print(f"> ¿3 y 4 son iguales? {c is e}") # Operador Identidad
+
+
+print(f"¿3 mayor que 2? {3 > 2}")
+
+
+
+print(f"Verdadero y Verdadero = {True and True}")
+print(f"Verdadero y 1 = {True and 1}")
+print(f"> Falso y 0 = {False and 0}")
+
+
+print(f"Not Verdadero = {not True}")
+print(f"Not Falso = {not False}")
+print(f"Not Falso o Verdadero = {not (False or True)}")
+
+mi_texto:str = "Lorem ipsum dolor sit amet consectetur adipiscing elit dui odio"
+mi_sub_texto: str = "amet"
+mi_otro_sub_texto: str = "pepito"
+
+print(f"> ¿amet está dentro del mi_texto?: {mi_sub_texto in mi_texto}")
+print(f"> ¿pepito no está dentro de mi_texto?: {mi_otro_sub_texto not in mi_texto}")
+
+edad = 30
+edad += 1
+
+
+puntos_obtenidos: int = 0
+print(f"> Los puntos obtenidos actuales son: {puntos_obtenidos}")
+puntos_obtenidos = puntos_obtenidos + 1
+print(f"> Los puntos obtenidos actuales son: {puntos_obtenidos}")
+puntos_obtenidos += 1
+print(f"> Has conseguido otro punto. Ahora tienes: {puntos_obtenidos}")
+
+  
+  
+  
+'''
+EJERCICIO 
+  - Por consola el usuario debe acertar un numero secreto
+  - Si acierta en el primer intento: Sus puntos se suman 5 puntos y se multiplica por 2
+  - Si acierta en el segundo intento: se suman 5 puntos
+  - Si acierta a la tercera se suma 2 puntos
+  - Si falla todas las veces se resta 2 puntos.
+'''
+
--- Python/README.md
+++ Python/README.md
@@ -17,7 +17,7 @@ Tipos de nombrar variables:
 
 Para tipar las variables se utiliza el siguiente comando newUser:str para definir un string. Esto no cambia nada de como se definen las variables en Python, ya que al intérprete le da igual. Para lo que se utiliza es para la documentación del código y que sea mucho más fácil de mantener a largo plazo.
 
-Con el siguiente código es como se puede dar formato e imprimir varias variables en Python
+Con el siguiente código es como se puede dar formato e imprimir varias variables en Python. A esto se le llama string template (print(f'..texto...{}'))
 ```sh
 user = 'Pepe'
 edad = 25
@@ -76,10 +76,66 @@ Sería un paso por "Valor" y no me modifica la lista de misNumeros. Sin embargo,
 misNumeros.sort()
 ```
 
-estoy haciendo un paso por "Referencia" y esto modificando mi lista de misNumeros
+estoy haciendo un paso por "Referencia" y esto modificando mi lista de misNumeros 
 
+<br />
+<br />
+<br />
 
 ## _Clase 22/09/2022_
 
+Todos los lenguajes de programacion estan basados en 3 paradigmas:
+
+ - Programacion estructurada
+ - Programacion orientada a objetos
+ - Programación funcional (no reutilización de variables)
 
 
+Hay 2 tipos de condicionales en todo lenguaje de programación (while y if)
+
+Python a nivel de ámbito funciona a través de la tabulación. 
+
+Tipos de operaciones:
+
+ - Asignación
+ - Operadores numericos (suma +, resta -, mult *, division /, division entera //, exponente ** y modulo %)
+ - Logicos
+  
+
+
+
+PALABRAS RESERVADAS DE PYTHON
+
+- and (operador logico para concatenar condiciones)
+- as ()
+- assert
+- break (para salir de un loop)
+- class (para definir un tipo propio. Por ejemplo tipo persona con atributos nombre, apellidos, email, etc. Además tiene unas acciones (o métodos) como comer, caminar, etc.)
+- continue (para salir de un bucle)
+- def (para definir funciones)
+- del
+- elif (comprobar otra condición despues de un if)
+- else (si ninguna condición se cumple realizar lo siguiente a else)
+- except (si falla el codigo de try hacer lo de except)
+- exec
+- finally (si queremos que si o si se ejecute algo cuando acaba el codigo)
+- for (para empezar un bucle)
+- global (para definir una variable global para todos los ámbitos)
+- if (para definir una condicion)
+- import (para importar modulos que se exportan de una libreria)
+- in (quiere decir dentro de algo que es iterable)
+- is (sirve para saber si un valor es de un tipo u otro)
+- lambda 
+- nonlocal
+- not (para negar una condición)
+- or (operador logico para definir que se cumplan una condición u otra)
+- pass (para que se salte una iteración)
+- raise
+- return (para definir lo que queremos que sea el output de una función)
+- try (prueba a ejecutar lineas de codigo y ver si da error)
+- while (ejecuta el bucle mientras se cumpla cierta condición)
+- with 
+- yield
+- True
+- False
+- None
