2f518cca998fe0302827dd4d91b4d076ac3a2f71
mimove <mimove14@gmail.com>
2022-09-29 10:05:09 +0200

    update reto2 medio

--- Python/mainRetos.py
+++ Python/mainRetos.py
@@ -393,15 +393,14 @@ buyMusic(shortListDiscs) """
 
 
 
-#-------------------------------------#
+#------------------------------------------------
 # Reto 2
 # El programa debe preguntar el artículo y su precio y añadirlo a una variable (diccionario u objeto literal), hasta que el usuario decida terminar
 # ("Introducir otro elemento al carrito (Si / No)".
 # Una vez el usuario decida no introducir más elementos al carrito, debe mostrar por pantalla la lista de la compra y el coste total.
-#-------------------------------------#
+#------------------------------------------------
 
 from retos.medio.reto2 import reto2
-
 
 reto2()
 
---
+++ Python/retos/medio/reto3.py
@@ -0,0 +1,12 @@
+#-------------------------------------#
+# Reto 3
+# El nuevo gobierno ha decidido replantear el sistema de pago de impuestos. Ha pensado que a partir de ahora:
+# si una persona es mayor de 16 años y menor de 70 ésta debe pagar impuestos.
+# En caso de no tener ingresos iguales o superiores a 800€ se acumulará una deuda mensual del 10%.
+# Si supera los 800€, pero no llega a los 2000€, deberá pagar un impuesto del 30% mensual
+# Si supera los 2000€ esta persona deberá pagar el 50% en concepto de impuestos
+# si la persona es menor de 16 años, no tiene que pagar impuestos
+# Escribe un programa capaz de calcular la cantidad de impuestos, o endeudamiento, de una lista de personas** durante un año entero (12 meses)
+#-------------------------------------#
+
+
