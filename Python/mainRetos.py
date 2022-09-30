05c87ead5a6748acb11caf0431cebbbd570ba6e7
Miguel <mimove14@gmail.com>
2022-09-30 11:42:54 +0200

    update reto15 medio

--- Python/mainRetos.py
+++ Python/mainRetos.py
@@ -741,16 +741,17 @@ print(f'La diferencia entre las fechas es de: {dDays} días, {dHours} horas, {dM
 
 
 
-#-------------------------------------#
+#------------------------------------------------
 # Reto 15
 # Partiendo del siguiente snippet:
 # numeros = [10,20,(1,3),30,50,69,(10,20),40]
 # cantidad = 0
 # Realiza un script que permita encontrar dentro de la lista aquellos elementos que sean tuplas. Cada vez que encuentre una tupla, deberá incrementarse la variable cantidad.
 # *** Pista: Haz uso de isinstance()
-#-------------------------------------#
+#------------------------------------------------
 
 
+""" import retos.medio.reto15 """
 
 
 
