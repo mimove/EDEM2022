e8b6475c513b30e067bcba2c38fc7496bcb7af78
Miguel <mimove14@gmail.com>
2022-10-07 12:47:10 +0200

    Update mainRetos.py

--- Python/mainRetos.py
+++ Python/mainRetos.py
@@ -1022,6 +1022,8 @@ print(secondSmaller([5,2,3,1,5,7,8,9,3,3,1,6,5])) """
 
 
 
+
+
 #-------------------------------------#
 # Reto 30
 # Investiga acerca de ast y convierte un String en una lista. Es decir, un string que representa una lista literalmente. Puedes usar este ejemplo:
