9103310754d439753e98b057dedae1ab063b2017
mimove <mimove14@gmail.com>
2022-10-17 23:53:57 +0200

    Changes in main.py Excercise 2 deliverable 1

--- Modulo_0-Fundamentos/Entregables/Ejercicio_2/main.py
+++ Modulo_0-Fundamentos/Entregables/Ejercicio_2/main.py
@@ -3,10 +3,11 @@ import csv
 import time
 import os
 import string
-import pandas as pd
 
 def word_count(str):
+  
     # Obtained from https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-12.php
+    
     counts = dict()
     words = str.split()
 
@@ -42,18 +43,21 @@ while True:
       datos = {"Personaje": datosjson[0]["character"].translate(str.maketrans('', '', string.punctuation)).split(), "Frase": datosjson[0]["quote"]}
         
       
-
+      # Imagen del personaje 
       img_personaje = datosjson[0]["image"]
       
-
-
+      
       # Obtenemos valor en la clave 'value' del JSON que nos interesa
         
       print('La frase de {} es: {}'.format(" ".join(datos["Personaje"]), datos["Frase"]))
       
       
+      # Actualizamos la lista de palabras con la cuenta de cada una
+      
       dictWords.update(word_count(string.capwords(datos["Frase"].translate(str.maketrans('', '', '".,:!?')))))
       
+      
+      # Escribimos la lista de palabras como clave, valor en cada fila de un csv
       
       with open('countedWords.csv', 'w') as csv_file:  
         writer = csv.writer(csv_file)
@@ -61,11 +65,13 @@ while True:
           writer.writerow([key, value])  
         
         
-      #Check if folder of character exist, if not create it.
+      # Para el directorio de cada personaje, primero comprobamos si existe.
         
       if not os.path.exists("_".join(datos["Personaje"])):
         os.makedirs("_".join(datos["Personaje"]))
         
+        
+        # Almacenamos la frase del personaje en un csv
         
         with open("_".join(datos["Personaje"]) + '/' + 'quotes' + '_' + "_".join(datos["Personaje"]) + '.csv', 'a', encoding='UTF8') as f:
           w = csv.DictWriter(f, datos.keys())
@@ -76,26 +82,18 @@ while True:
           # write the data
           w.writerow({'Personaje':" ".join(datos["Personaje"]), 'Frase': datos["Frase"]})
         
-        img_data = requests.get(img_personaje).content
+        # Descargamos la imagen del personaje.
+        
         with open("_".join(datos["Personaje"]) + '/' + "_".join(datos["Personaje"]) +'.png', 'wb') as handler:
           if handler.tell() == 0:
+            img_data = requests.get(img_personaje).content
             handler.write(img_data)
 
           
-          
-        
-
-        # with open('Palabras/quotes_general.csv', 'a', encoding='UTF8') as f:
-        #   w = csv.DictWriter(f, datos.keys())
-            
-        #   if f.tell() == 0:
-        #     w.writeheader()
-
-        #     # write the data
-        #     w.writerow(datos)
 
 
        
+        # Tiempo de espera para siguiente iteración
         
         time.sleep(1)
 
