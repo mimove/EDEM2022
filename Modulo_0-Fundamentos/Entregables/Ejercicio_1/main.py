7bcef9ce6ceb837d7f14036b1d07c786e4dd0830
mimove <mimove14@gmail.com>
2022-10-23 20:40:32 +0200

    Changing Ex1 and Ex2 to be cleaner and Ex3 to plot in Jupyter each character image in a dynamic subplot

--- Modulo_0-Fundamentos/Entregables/Ejercicio_1/main.py
+++ Modulo_0-Fundamentos/Entregables/Ejercicio_1/main.py
@@ -2,56 +2,61 @@ import requests
 import csv
 import time
 
+
+def writeData(path, mode, datos):
+    with open(path, mode, encoding='UTF8') as f:
+        w = csv.DictWriter(f, datos.keys())
+        
+        # If file doesn't exist, create header
+        if f.tell() == 0:
+            w.writeheader()
+
+        # write the data
+        w.writerow(datos)
+    
+
+
 while True:
    
     URL = 'https://thesimpsonsquoteapi.glitch.me/quotes'
 
 
-    respuesta = requests.get(URL)
+    # Trying several times to connect to the API to avoid a freezing problem that occurs every now and then.
+    try:
+      respuesta = requests.get(URL,timeout=3)
+    except requests.exceptions.Timeout as err:
+      print(err)
+      time.sleep(1)
+      
 
     if respuesta.status_code >= 200 and respuesta.status_code < 300:
 
-        # Extraemos los datos en formato JSON
+        # Extract data in json format
         
         datosjson = respuesta.json()
+        
+        # Store data in a dict with the name of the character and the quote
         
         datos = {"Personaje": datosjson[0]["character"], "Frase": datosjson[0]["quote"]}
 
 
-        # Obtenemos valor en la clave 'value' del JSON que nos interesa
+        # Printing values in dict as a checking step
         
         print('La frase de {} es: {}'.format(datos["Personaje"], datos["Frase"]))
 
-        with open('General/quotes_general.csv', 'a', encoding='UTF8') as f:
-            w = csv.DictWriter(f, datos.keys())
-            
-            if f.tell() == 0:
-               w.writeheader()
 
-            # write the data
-            w.writerow(datos)
+        # Use of writeData function to write quotes in a csv file
+        
+        writeData('General/quotes_general.csv', 'a', datos)
 
 
         if 'Homer' in datos["Personaje"]:
-          with open('Homer/quotes_homer.csv', 'a', encoding='UTF8') as f:
-              w = csv.DictWriter(f, datos.keys())
-              
-              if f.tell() == 0:
-                  w.writeheader()
-
-              # write the data
-              w.writerow(datos)
+            writeData('Homer/quotes_homer.csv', 'a', datos)
 
         elif 'Lisa' in datos["Personaje"]:
+            writeData('Lisa/quotes_lisa.csv', 'a', datos)
 
-           with open('Lisa/quotes_lisa.csv', 'a', encoding='UTF8') as f:
 
-              w = csv.DictWriter(f, datos.keys())
-              
-              if f.tell() == 0:  
-                  w.writeheader()
-
-              # write the data
-              w.writerow(datos)
+        # I choose 1 second so that the verification steps are much faster
         
         time.sleep(1)
--- Modulo_0-Fundamentos/Entregables/Ejercicio_3/python/main.py
+++ Modulo_0-Fundamentos/Entregables/Ejercicio_3/python/main.py
@@ -4,9 +4,20 @@ import time
 import os
 import string
 
+def writeData(path, mode, datos):
+    with open(path, mode, encoding='UTF8') as f:
+        w = csv.DictWriter(f, datos.keys())
+        
+        # If file doesn't exist, create header
+        if f.tell() == 0:
+            w.writeheader()
+
+        # write the data
+        w.writerow(datos)
+
 def word_count(str,dictCount):
   
-    # Obtained from https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-12.php
+    # Modified from https://www.w3resource.com/python-exercises/string/python-data-type-string-exercise-12.php
     
     counts = dictCount
     words = str.split()
@@ -22,11 +33,11 @@ def word_count(str,dictCount):
 
 dictWords = dict()
 
-
 while True:
    
     URL = 'https://thesimpsonsquoteapi.glitch.me/quotes'
 
+    # Trying several times to connect to the API to avoid a freezing problem that occurs every now and then.
     try:
       respuesta = requests.get(URL,timeout=3)
     except requests.exceptions.Timeout as err:
@@ -35,30 +46,32 @@ while True:
       
     if respuesta.status_code >= 200 and respuesta.status_code < 300:
 
-      # Extraemos los datos en formato JSON
+      # Extract data in json format
         
       datosjson = respuesta.json()
       
-      # a_string.translate(str.maketrans('', '', string.punctuation)
+      
+      # Store data in a dict, but name of character as a list of words for using them later in dynamic directory creation
       
       datos = {"Personaje": datosjson[0]["character"].translate(str.maketrans('', '', string.punctuation)).split(), "Frase": datosjson[0]["quote"]}
         
       
-      # Imagen del personaje 
+      # URL of image of the character
+      
       img_personaje = datosjson[0]["image"]
       
       
-      # Obtenemos valor en la clave 'value' del JSON que nos interesa
+      # Printing values in dict as a checking step
         
-      print('La frase de {} es: {}'.format(" ".join(datos["Personaje"]), datos["Frase"]))
+      print('La frase de {} es: {}'.format(' '.join(datos["Personaje"]), datos["Frase"]))
       
       
-      # Actualizamos la lista de palabras con la cuenta de cada una
+      # Updating dictionary with the word count. Removing punctuation characters except single quotation marks (') to avoid mixing words like we're with were.
       
-      dictWords.update(word_count(string.capwords(datos["Frase"].translate(str.maketrans('', '', '".,:!?'))), dictWords))
+      dictWords.update(word_count(string.capwords(datos["Frase"].translate(str.maketrans('', '', '".,:!?'))),dictWords))
       
       
-      # Escribimos la lista de palabras como clave, valor en cada fila de un csv
+      # Writing words as a list of key, value in a csv file in every row
       
       with open('results/countedWords.csv', 'w') as csv_file:  
         writer = csv.writer(csv_file)
@@ -68,35 +81,26 @@ while True:
           writer.writerow([key, value])  
         
         
-      # Para el directorio de cada personaje, primero comprobamos si existe.
+      # To create a directory for every character, we need to first check if it already exists. If it doesn't, the directory is created
+      
+      if not os.path.exists('results/'+'_'.join(datos["Personaje"])):
+        os.makedirs('results/'+'_'.join(datos["Personaje"]))
         
-      if not os.path.exists("results/"+"_".join(datos["Personaje"])):
-        os.makedirs("results/"+"_".join(datos["Personaje"]))
+        
+        # Storing the word of the character in a csv file with its name
+        
+        writeData('results/'+'_'.join(datos["Personaje"]) + '/' + 'quotes' + '_' + '_'.join(datos["Personaje"]) + '.csv', 'a', {'Personaje':' '.join(datos["Personaje"]), 'Frase': datos["Frase"]})
         
         
-        # Almacenamos la frase del personaje en un csv
+        # Download the image of the character in its folder
         
-        with open("results/"+"_".join(datos["Personaje"]) + '/' + 'quotes' + '_' + "_".join(datos["Personaje"]) + '.csv', 'a', encoding='UTF8') as f:
-          w = csv.DictWriter(f, datos.keys())
-          
-          if f.tell() == 0:
-            w.writeheader()
-
-          # write the data
-          w.writerow({'Personaje':" ".join(datos["Personaje"]), 'Frase': datos["Frase"]})
-        
-        # Descargamos la imagen del personaje.
-        
-        with open("results/"+"_".join(datos["Personaje"]) + '/' + "_".join(datos["Personaje"]) +'.png', 'wb') as handler:
+        with open('results/'+'_'.join(datos["Personaje"]) + '/' + '_'.join(datos["Personaje"]) +'.png', 'wb') as handler:
           if handler.tell() == 0:
             img_data = requests.get(img_personaje).content
             handler.write(img_data)
 
-          
-
-
        
-        # Tiempo de espera para siguiente iteración
+        # I choose 1 second so that the verification steps are much faster
         
         time.sleep(1)
 
